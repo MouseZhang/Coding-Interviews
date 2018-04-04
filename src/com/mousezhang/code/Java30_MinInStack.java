f11f91fefb1b4e67826dbe9ed9cec434f4464e0d
MouseZhang <athope@163.com>
2018-04-04 23:37:08 +0800

    MinInStack

--- src/com/mousezhang/code/Java30_MinInStack.java
+++ src/com/mousezhang/code/Java30_MinInStack.java
@@ -7,7 +7,43 @@
  * // 题目：定义栈的数据结构，请在该类型中实现一个能够得到栈最小元素的min函数。
  */
 package com.mousezhang.code;
+import java.util.Stack;
 
 public class Java30_MinInStack {
-
+	Stack<Integer> m_data = new Stack<Integer>();
+	Stack<Integer> m_min = new Stack<Integer>();
+    
+	public void push(int node) {
+		if (m_min.isEmpty()) {
+		    m_min.push(node);
+		}
+		else {
+		    int top = m_min.peek();
+		    if (node < top) {
+		        m_min.push(node);
+		    }
+		    else {
+		        m_min.push(top);
+		    }
+		}
+		m_data.push(node);
+	}
+    
+	public void pop() {
+		if (!m_data.isEmpty()) {
+		    m_data.pop();
+		    m_min.pop();
+		}
+	}
+    
+	public int top() {
+		return m_data.peek();
+	}
+    
+	public int min() {
+		if (m_min.isEmpty()) {
+		    return 0;
+		}
+		return m_min.peek();
+	}
 }
