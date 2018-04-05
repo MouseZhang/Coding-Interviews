470c92c39eb1b6486b81f5df150781e80b56bb98
MouseZhang <athope@163.com>
2018-04-05 13:23:03 +0800

    BranchPrintTree

--- src/com/mousezhang/code/Java32_02_BranchPrintTree.java
+++ src/com/mousezhang/code/Java32_02_BranchPrintTree.java
@@ -9,36 +9,40 @@
 package com.mousezhang.code;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class Java32_02_BranchPrintTree {
 	public ArrayList<ArrayList<Integer> > Print(TreeNode pRoot) {
 		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-		if (pRoot == null) {
-			return result;
-        }
-		ArrayList<Integer> temp = new ArrayList<>();
-		LinkedList<TreeNode> queue = new LinkedList<>();
-		queue.offer(pRoot);
-		TreeNode node = null;
-		TreeNode last = pRoot;
-		TreeNode nlast = last;
-		while (!queue.isEmpty()) {
-			node = queue.poll();
-			temp.add(node.val);
-			if (node.left != null) {
-			    queue.offer(node.left);
-			    nlast = node.left;
-			}
-			if (node.right != null) {
-			    queue.offer(node.right);
-			    nlast = node.right;
-			}
-			if (node == last) {
-				result.add(temp);
-			    temp = new ArrayList<Integer>();
-			    last = nlast;
-			}
-        }
+		if(pRoot == null){
+		    return result;
+		}
+		//使用队列，先进先出
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		ArrayList<Integer> tempList = new ArrayList<Integer>();
+		queue.add(pRoot);
+		//start记录本层打印了多少个，end记录下一层要打印多少个
+		int start = 0, end = 1;
+		while(!queue.isEmpty()){
+		    TreeNode cur = queue.remove();
+		  //添加本行打印的List里
+		    tempList.add(cur.val);
+		    start++;
+		    //每打印一个节点，就把此节点的下一层的左右节点加入队列，并记录下一层要打印的个数
+		    if(cur.left!=null){
+		    	queue.add(cur.left);           
+		    }
+		    if(cur.right!=null){
+		    	queue.add(cur.right);
+		    }
+		    //本层打印完毕
+		    if(start == end){
+		        end = queue.size();
+		        start = 0;
+		        result.add(tempList);
+		        tempList = new ArrayList<Integer>();
+		    }
+		}
 		return result;
-    }
+	}
 }
