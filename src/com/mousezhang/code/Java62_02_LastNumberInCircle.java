c63c7220291bd1ffd97bd95ba598abd4b1e53159
MouseZhang <athope@163.com>
2018-04-10 14:03:47 +0800

    LastNumberInCircle

--- src/com/mousezhang/code/Java62_02_LastNumberInCircle.java
+++ src/com/mousezhang/code/Java62_02_LastNumberInCircle.java
@@ -8,7 +8,25 @@
  * // 删除第m个数字。求出这个圆圈里剩下的最后一个数字。
  */
 package com.mousezhang.code;
+import java.util.ArrayList;
 
 public class Java62_02_LastNumberInCircle {
-
+	/** 方法二：链表,O(N) */
+    public int LastRemaining_Solution(int n, int m) {
+        if (m == 0 || n == 0) {
+            return -1;
+        }
+        ArrayList<Integer> data = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            data.add(i);
+        }
+        int index = -1;
+        while (data.size() > 1) {
+        	//重点是此步
+            index = (index + m) % data.size();
+            data.remove(index);
+            index--;
+        }
+        return data.get(0);
+    }
 }
