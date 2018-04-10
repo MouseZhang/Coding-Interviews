a06731b9747bdd686686994c66945766285ace4e
MouseZhang <athope@163.com>
2018-04-10 14:04:28 +0800

    LastNumberInCircle

--- src/com/mousezhang/code/Java62_02_LastNumberInCircle.java
+++ src/com/mousezhang/code/Java62_02_LastNumberInCircle.java
@@ -12,21 +12,21 @@ import java.util.ArrayList;
 
 public class Java62_02_LastNumberInCircle {
 	/** 方法二：链表,O(N) */
-    public int LastRemaining_Solution(int n, int m) {
-        if (m == 0 || n == 0) {
-            return -1;
-        }
-        ArrayList<Integer> data = new ArrayList<Integer>();
-        for (int i = 0; i < n; i++) {
-            data.add(i);
-        }
-        int index = -1;
-        while (data.size() > 1) {
-        	//重点是此步
-            index = (index + m) % data.size();
-            data.remove(index);
-            index--;
-        }
-        return data.get(0);
-    }
+	public int LastRemaining_Solution(int n, int m) {
+		if (m == 0 || n == 0) {
+		    return -1;
+		}
+		ArrayList<Integer> data = new ArrayList<Integer>();
+		for (int i = 0; i < n; i++) {
+		    data.add(i);
+		}
+		int index = -1;
+		while (data.size() > 1) {
+			//重点是此步
+		    index = (index + m) % data.size();
+		    data.remove(index);
+		    index--;
+		}
+		return data.get(0);
+	}
 }
